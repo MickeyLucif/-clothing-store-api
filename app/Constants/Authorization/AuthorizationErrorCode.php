6a92438d952fd4c4e6edf3e5a5b883dc37cdbf6b
MickeyLucif <kbelova241@gmail.com>
2026-09-08 17:56:15 +0300

    Add shared error codes

---
+++ app/Constants/Authorization/AuthorizationErrorCode.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Constants\Authorization;
+
+final class AuthorizationErrorCode
+{
+    public const NO_PERMISSIONS_AVAILABLE_FOR_GUARD = 'NO_PERMISSIONS_AVAILABLE_FOR_GUARD';
+
+    private function __construct() {}
+}
